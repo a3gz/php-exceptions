621316f8090876d1553f153a3fac671ac79951c0
Alejandro Arbiza <alejandro@roetal.com>
2019-04-28 23:02:05 -0400

    Add `internalServerError` to `HttpException`
    	modified:   src/HttpException.php

--- src/HttpException.php
+++ src/HttpException.php
@@ -7,6 +7,10 @@ class HttpException extends AbstractHttpException
     return new static($msg, 400);
   }
 
+  public static function internalServerError($msg = 'Internal server error') {
+    return new static($msg, 500);
+  }
+
   public static function notFound($msg = 'Not found') {
     return new static($msg, 404);
   }
