178dcc2e01fa12e76206c4977289c15eb2c110dd
Alejandro Arbiza <alejandro@roetal.com>
2019-05-02 07:35:55 -0400

    Fix missing argument in error throwing
    	modified:   src/HttpException.php

--- src/HttpException.php
+++ src/HttpException.php
@@ -4,15 +4,15 @@ namespace A3gZ\PhpExceptions;
 class HttpException extends AbstractException
 {
   public static function badRequest($msg = 'Bad Request') {
-    return new static($msg, 400);
+    return new static($msg, 'http_bad_request', 400);
   }
 
   public static function internalServerError($msg = 'Internal server error') {
-    return new static($msg, 500);
+    return new static($msg, 'http_internal_server_error', 500);
   }
 
   public static function notFound($msg = 'Not found') {
-    return new static($msg, 404);
+    return new static($msg, 'http_not_found', 404);
   }
 } // class
 
